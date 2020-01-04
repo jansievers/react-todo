b87b7654d00bb20aaa52336c52b3c17551faf403
Jan-Christoph Sievers <jcs@jan-christoph-sievers.de>
2020-01-04 19:50:38 +0100

    TodoSearch

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,10 +2,13 @@ const React = require('react');
 
 const TodoList = require('TodoList');
 const AddTodo = require('AddTodo');
+const TodoSearch = require('TodoSearch');
 
 const TodoApp = React.createClass({
     getInitialState: function() {
       return {
+        showCompleted: false,
+        searchText: '',
         todos: [
             {
                 id: 1,
@@ -29,10 +32,17 @@ const TodoApp = React.createClass({
     handleAddTodo: function(todoName) {
         alert(todoName);
     },
+    handleSearch: function(showCompleted, searchText) {
+        this.setState({
+            showCompleted: showCompleted,
+            searchText: searchText.toLowerCase()
+        });
+    },
     render: function () {
         const {todos} = this.state;
         return(
             <div>
+                <TodoSearch onSearch={this.handleSearch}/>
                 <TodoList todos={todos}/>
                 <AddTodo onHandleAddTodo={this.handleAddTodo} />
             </div>
