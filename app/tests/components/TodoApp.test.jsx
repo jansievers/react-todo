af037dd2e6afeb4fe0faebac1f91ac45d5550bad
Jan-Christoph Sievers <jcs@jan-christoph-sievers.de>
2020-01-05 19:21:04 +0100

    timestamps

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -28,11 +28,13 @@ describe('TodoApp', () => {
     todoApp.setState({todos: []});
     todoApp.handleAddTodo(todoText);
 
-    expect(todoApp.state.todos[0].text).toBe(todoText);
+    const todo = todoApp.state.todos[0];
+    expect(todo.text).toBe(todoText);
+    expect(todo.createdAt).toBeA('number');
   });
 
   it('should toggle', () => {
-    const todoData = {id: 11, text: 'Test features', completed: false};
+    const todoData = {id: 11, text: 'Test features', completed: false, createdAd: 0, completedAd: undefined};
 
     const todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
     todoApp.setState({todos:[todoData]});
@@ -40,5 +42,19 @@ describe('TodoApp', () => {
     expect(todoApp.state.todos[0].completed).toBe(false);
     todoApp.handleToggle(11);
     expect(todoApp.state.todos[0].completed).toBe(true);
+    expect(typeof todoApp.state.todos[0].completedAt === 'number').toBe(true);
   });
+
+  it('should toggle incomplete', () => {
+    const todoData = {id: 11, text: 'Test features', completed: true, createdAd: 0, completedAd: 123};
+
+    const todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos:[todoData]});
+
+    expect(todoApp.state.todos[0].completed).toBe(true);
+    todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    expect(todoApp.state.todos[0].completedAt).toNotExist();
+  });
+
 });
