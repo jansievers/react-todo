3b8a0c6656b775a9cb29fbf30cbffb62784da019
Jan-Christoph Sievers <jcs@jan-christoph-sievers.de>
2020-01-18 21:42:30 +0100

    Reducers and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -24,4 +24,34 @@ describe('Reducers', () => {
             expect(res).toEqual(true);
         });
     });
+
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            const action = {
+                type: 'ADD_TODO',
+                text: 'Gym'
+            };
+            const res = reducers.todosReducer(df([]), df(action));
+            console.log('*** res ***', res.length);
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+
+        it('should toggle a todo', () => {
+            let todos = [{
+                id: '123',
+                text: 'Something',
+                completed: true,
+                createdAt: 123,
+                completedAt: 125
+            }];
+            const action = {
+                type: 'TOGGLE_TODO',
+                id: '123'
+            };
+            const res = reducers.todosReducer(df(todos), df(action));
+            expect(res[0].completed).toEqual(false);
+            expect(res[0].completedAt).toEqual(undefined);
+        });
+    });
 });
