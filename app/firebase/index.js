a3c1054936124a49db23359b6ca379664ed3416d
Jan-Christoph Sievers <jcs@jan-christoph-sievers.de>
2020-02-02 16:41:20 +0100

    Setup envs

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,13 +3,13 @@ import firebase from 'firebase';
 try {
     // Your web app's Firebase configuration
     const firebaseConfig = {
-        apiKey: "AIzaSyBh5SHUMtdWAY_PzKHSJ2cZwaC4Fto0swk",
-        authDomain: "angular-tutorial-8a968.firebaseapp.com",
-        databaseURL: "https://angular-tutorial-8a968.firebaseio.com",
-        projectId: "angular-tutorial-8a968",
-        storageBucket: "angular-tutorial-8a968.appspot.com",
-        messagingSenderId: "327932558336",
-        appId: "1:327932558336:web:fa89feafa4d9a6317a2ce5"
+        apiKey: process.env.API_KEY, // "AIzaSyBh5SHUMtdWAY_PzKHSJ2cZwaC4Fto0swk",
+        authDomain: process.env.AUTH_DOMAIN,
+        databaseURL: process.env.DATABASE_URL,
+        projectId: process.env.PROJECT_ID,
+        storageBucket: process.env.STORAGE_BUCKET,
+        messagingSenderId: process.env.MESSAGING_SENDER_ID,
+        appId: process.env.APP_ID
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
