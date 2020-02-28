219d11b8d07ffe4b308270130daf8e32f8b87121
Jan Sievers <jcs@facelift-bbt.com>
2020-02-28 16:30:43 +0100

    - no login yet, fix node-sass

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,10 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const {Provider} = require('react-redux');
-const {Route, Router, IndexRoute, hashHistory} = require('react-router');
 const TodoApp = require('TodoApp');
 
 const actions = require('actions');
 const store = require('configureStore').configure();
-const TodoAPI = require('TodoAPI');
-import Login from 'Login';
 
 store.dispatch(actions.startAddTodos());
 
@@ -19,21 +16,7 @@ require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={hashHistory}>
-            <Route path="/">
-                <Route path="todos" component={TodoApp}/>
-                <IndexRoute component={Login}/>
-            </Route>
-        </Router>
-    </Provider>,
-    document.getElementById('app')
-);
-
-/*
-ReactDOM.render(
-    <Provider store={store}>
         <TodoApp/>
     </Provider>,
     document.getElementById('app')
 );
-*/
